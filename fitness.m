b4b83e55380261a0dff4ee3b1e0578aca2e4a1a7
Jan Korcek <korcekdevelopment@yahoo.com>
2019-03-05 08:57:57 +0100

    Comments have been edited.

--- fitness.m
+++ fitness.m
@@ -3,13 +3,13 @@
 
 function[Fit]=fitness(Pop, x, y)
 
-    [lpop,lstring]=size(Pop); % Zistenie velkosti matrice Pop -> pocet riadkov, stlpcov
+    [lpop,lstring]=size(Pop); % zistenie velkosti matrice Pop -> pocet riadkov, stlpcov
     
     for i = 1:lpop
-        alphas=Pop(i,:);
-        Fx(i,:)=polyval(alphas, x); % Vypocet funkcnych hodnot pre kazde x
-        diff=abs(Fx(i,:)-y); % Vypocet rozdielu funkcnych hodnot 
-        Fit(i)=sum(diff(:));
+        alphas=Pop(i,:); % vybranie jedneho jedinca z populacie
+        Fx(i,:)=polyval(alphas, x); % vypocet funkcnych hodnot pre kazde x ako parameter
+        diff=abs(Fx(i,:)-y); % vypocet rozdielu funkcnych hodnot 
+        Fit(i)=sum(diff(:)); % priradenie rozdielu funkcnych hodnot na riadok i
     end
     
 end
--- regression.m
+++ regression.m
@@ -1,6 +1,6 @@
-xS=[1 2 4 6];
-yS=[1.005 3.94 1.62 4.58];
-alphas=[0.075 -0.57 0 5.8 -4.3];
+xS=[1 2 4 6]; % x-hodnoty
+yS=[1.005 3.94 1.62 4.58]; % y-hodnoty
+alphas=[0.075 -0.57 0 5.8 -4.3]; % predpokladane hodnoty alpha
 
 wx=-6:0.1:8;
 
@@ -12,31 +12,22 @@ Pop=genrpop(lpop,Space); % generovanie populacie z daneho rozsahu
 Fit=fitness(Pop, xS, yS); % vyhodnotenie jedincov fitness funkciou
 [minFit, indx]=min(Fit); % najdenie najlepsieho (minimalneho) retazca
 minRet=Pop(indx,:); % ulozenie najlepsieho retazca do pomocnej premennej
-grafFit=zeros(1,numcykle);
+grafFit=zeros(1,numcykle); % vytvorenie grafu pre zobrazenie genetickeho algoritmu
 
 for i=1:numcykle
-%     NewPop1=selbest(Pop, Fit, [3 1]); % 3 najlepsich a 1 nahodny vyber prvkov
-%     NewPop2=selrand(Pop, Fit, 26); % nahodne vybrane ostatne chybajuce prvky do poppulacie
-%     
-%     CrossedPop=crossov(NewPop2, 2, 0); % krizenie
-%     MutedPopX=mutx(CrossedPop, 0.2, Space); % mutacia
-%     MutedPopA=muta(MutedPopX, 0.05, [8 8 8 8 8]*0.05, Space); % mutacia
-%     
-%     Pop=[NewPop1; MutedPopA]; % Spojenie najlepsich a zmutovanych jedincov
-
-    NewPop1 = selbest(Pop, Fit, [2 0]); 
-    NewPop3 = seltourn(Pop, Fit, 13);
-    NewPopBest = [NewPop1; NewPop3]; 
-    CrossedPop1=crossov(NewPopBest, 2, 0); % krizenie
-    MutedPopX1=mutx(CrossedPop1, 0.1, Space); % mutacia
-    MutedPopA1=muta(MutedPopX1, 0.05, [6 6 6 6 6]*0.05, Space); % mutacia
+    NewPop1 = selbest(Pop, Fit, [2 0]); % vyber 2 najlepsich jedincov
+    NewPop3 = seltourn(Pop, Fit, 13); % vyber 13 jedincov turnajovym vyberom
+    NewPopBest = [NewPop1; NewPop3]; % spojenie vybranych populacii do matice
+    CrossedPop1=crossov(NewPopBest, 2, 0); % nahodne krizenie dvoch parentov
+    MutedPopX1=mutx(CrossedPop1, 0.1, Space); % globalna mutacia
+    MutedPopA1=muta(MutedPopX1, 0.05, [6 6 6 6 6]*0.05, Space); % aditivna mutacia
     
-    NewPopRand=selrand(Pop, Fit, 15); % nahodne vybrane ostatne chybajuce prvky do populacie
-    CrossedPop2=crossov(NewPopRand, 2, 0); % krizenie
-    MutedPopX2=mutx(CrossedPop2, 0.25, Space); % mutacia
-    MutedPopA2=muta(MutedPopX2, 0.05, [6 6 6 6 6]*0.05, Space); % mutacia
+    NewPopRand=selrand(Pop, Fit, 15); % nahodne vybrane ostatne chybajuce jedince do populacie
+    CrossedPop2=crossov(NewPopRand, 2, 0); % nahodne krizenie dvoch parentov
+    MutedPopX2=mutx(CrossedPop2, 0.25, Space); % globalna mutacia
+    MutedPopA2=muta(MutedPopX2, 0.05, [6 6 6 6 6]*0.05, Space); % aditivna mutacia
     
-    Pop=[MutedPopA1; MutedPopA2]; % Spojenie najlepsich a zmutovanych jedincov
+    Pop=[MutedPopA1; MutedPopA2]; % Spojenie zmutovanych a skrizenych jedincov do matice
 
     Fit=fitness(Pop, xS, yS); % znovu vyhodnotenie fitness funkcie
     [minFitnew,indx]=min(Fit); % najdenie najlepsieho (minimalneho) retazca
